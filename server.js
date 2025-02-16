f41805bea355a046da87312e747cea89aff5c289
Mahmoud <mhmodq717@gmail.com>
2025-02-16 07:41:08 +0200

    6th commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", () => {});
+const port = 4000;
 app.listen(port, () => {
   console.log(`server listens on port ${port}`);
 });
