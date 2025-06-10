4a1921cfbf651b5744302a34dd719f63f3989f1c
Mahmoud <mhmodq717@gmail.com>
2025-06-10 09:34:12 +0300

    mail service fixed

--- app/modules/user/services/emailService.js
+++ app/modules/user/services/emailService.js
@@ -1,54 +1,15 @@
-// const nodemailer = require("nodemailer");
-// const bcrypt = require("bcryptjs");
-// const generateResetEmail = require("../../utils/emailTemplate"); // Import HTML email template
-// require("dotenv").config();
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_PASS,
-//   },
-// });
-
-// /**
-//  * @param {string} email
-//  * @returns {Promise<string>}
-//  */
-// const sendPasswordResetCode = async (email) => {
-//   try {
-//     const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
-//     const hashedCode = await bcrypt.hash(resetCode, 10);
-
-//     // Use the HTML email template
-//     const emailContent = generateResetEmail(resetCode);
-
-//     await transporter.sendMail({
-//       from: `"YourApp Support" <${process.env.GMAIL_USER}>`,
-//       to: email,
-//       subject: "Reset Your Password",
-//       html: emailContent, // Use HTML instead of plain text
-//     });
-
-//     console.log(`OTP sent to ${email}: ${resetCode}`);
-//     return hashedCode;
-//   } catch (error) {
-//     console.error("Email Sending Error:", error);
-//     throw new Error("Failed to send email");
-//   }
-// };
-
-// module.exports = {
-//   sendPasswordResetCode,
-// };
-
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 const bcrypt = require("bcryptjs");
-const generateResetEmail = require("../../utils/emailTemplate");
+const generateResetEmail = require("../../utils/emailTemplate"); // Import HTML email template
 require("dotenv").config();
 
-// const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
+});
 
 /**
  * @param {string} email
@@ -58,26 +19,21 @@ const sendPasswordResetCode = async (email) => {
   try {
     const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
     const hashedCode = await bcrypt.hash(resetCode, 10);
+
+    // Use the HTML email template
     const emailContent = generateResetEmail(resetCode);
 
-    const msg = {
+    await transporter.sendMail({
+      from: `"YourApp Support" <${process.env.GMAIL_USER}>`,
       to: email,
-      from: "diadiagnotech123@gmail.com",
       subject: "Reset Your Password",
-      html: emailContent,
-    };
+      html: emailContent, // Use HTML instead of plain text
+    });
 
-    await sgMail.send(msg);
-
-    console.log(`Password reset email sent to ${email}.`);
+    console.log(`OTP sent to ${email}: ${resetCode}`);
     return hashedCode;
   } catch (error) {
     console.error("Email Sending Error:", error);
-
-    if (error.response) {
-      console.error(error.response.body);
-    }
-
     throw new Error("Failed to send email");
   }
 };
